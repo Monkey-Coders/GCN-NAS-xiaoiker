f70cc14f81d8a5e61eba1f3e79c1a32ec75597c7
Zaim Imran <zaim.imran@gmail.com>
2023-02-22 13:07:45 +0100

    zc

---
+++ mainzc.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+import json
+import os
+import time
+import numpy as np
+import yaml
+import pickle
+from collections import OrderedDict
+# torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from tqdm import tqdm
+from ZeroCostFramework.zero_cost_controller import calculate_zc_proxy_scores
+from tensorboardX import SummaryWriter
+import shutil
+from torch.optim.lr_scheduler import ReduceLROnPlateau, MultiStepLR
+import random
+import inspect
+import torch.backends.cudnn as cudnn
+import wandb
+from create_random_architectures import get_model_hash
+import json
+
+wandb.init(project="zaim-train", entity="gcn-nas")
+
+
+def init_seed(_):
+    torch.cuda.manual_seed_all(1)
+    torch.manual_seed(1)
+    np.random.seed(1)
+    random.seed(1)
+    # torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def get_parser():
+    # parameter priority: command line > config > default
+    parser = argparse.ArgumentParser(
+        description='Spatial Temporal Graph Convolution Network')
+    parser.add_argument(
+        '--work-dir',
+        default='./work_dir/temp',
+        help='the work folder for storing results')
+
+    parser.add_argument('-model_saved_name', default='')
+    parser.add_argument(
+        '--config',
+        default='',#./config/nturgbd-cross-view/test_bone.yaml
+        help='path to the configuration file')
+
+    # processor
+    parser.add_argument(
+        '--phase', default='train', help='must be train or test')
+    parser.add_argument(
+        '--save-score',
+        type=str2bool,
+        default=False,
+        help='if ture, the classification score will be stored')
+
+    # visulize and debug
+    parser.add_argument(
+        '--seed', type=int, default=1, help='random seed for pytorch')
+    parser.add_argument(
+        '--log-interval',
+        type=int,
+        default=100,
+        help='the interval for printing messages (#iteration)')
+    parser.add_argument(
+        '--save-interval',
+        type=int,
+        default=2,
+        help='the interval for storing models (#iteration)')
+    parser.add_argument(
+        '--eval-interval',
+        type=int,
+        default=5,
+        help='the interval for evaluating models (#iteration)')
+    parser.add_argument(
+        '--print-log',
+        type=str2bool,
+        default=True,
+        help='print logging or not')
+    parser.add_argument(
+        '--show-topk',
+        type=int,
+        default=[1, 5],
+        nargs='+',
+        help='which Top K accuracy will be shown')
+
+    # feeder
+    parser.add_argument(
+        '--feeder', default='feeder.feeder', help='data loader will be used')
+    parser.add_argument(
+        '--num-worker',
+        type=int,
+        default=32,
+        help='the number of worker for data loader')
+    parser.add_argument(
+        '--train-feeder-args',
+        default=dict(),
+        help='the arguments of data loader for training')
+    parser.add_argument(
+        '--test-feeder-args',
+        default=dict(),
+        help='the arguments of data loader for test')
+
+    # model
+    parser.add_argument('--model', default=None, help='the model will be used')
+    parser.add_argument(
+        '--model-args',
+        type=dict,
+        default=dict(),
+        help='the arguments of model')
+    parser.add_argument(
+        '--weights',
+        default='',#./runs/ntu_cv_agcn_joint_Re-49-23500.pt #ntu_cv_agcn_joint_Re2_w64-69-21761.pt  
+        help='the weights for network initialization')
+    parser.add_argument(
+        '--ignore-weights',
+        type=str,
+        default=[],
+        nargs='+',
+        help='the name of weights which will be ignored in the initialization')
+
+    # optim
+    parser.add_argument(
+        '--base-lr', type=float, default=0.01, help='initial learning rate')
+    parser.add_argument(
+        '--step',
+        type=int,
+        default=[20, 40, 60],
+        nargs='+',
+        help='the epoch where optimizer reduce the learning rate')
+    parser.add_argument(
+        '--device',
+        type=int,
+        default=0,
+        nargs='+',
+        help='the indexes of GPUs for training or testing')
+    parser.add_argument('--optimizer', default='SGD', help='type of optimizer')
+    parser.add_argument(
+        '--nesterov', type=str2bool, default=False, help='use nesterov or not')
+    parser.add_argument(
+        '--batch-size', type=int, default=256, help='training batch size')
+    parser.add_argument(
+        '--test-batch-size', type=int, default=256, help='test batch size')
+    parser.add_argument(
+        '--start-epoch',
+        type=int,
+        default=0,
+        help='start training from which epoch')
+    parser.add_argument(
+        '--num-epoch',
+        type=int,
+        default=80,
+        help='stop training in which epoch')
+    parser.add_argument(
+        '--weight-decay',
+        type=float,
+        default=0.0005,
+        help='weight decay for optimizer')
+    parser.add_argument('--only_train_part', default=False)
+    parser.add_argument('--only_train_epoch', default=0)
+    parser.add_argument('--warm_up_epoch', default=0)
+    return parser
+
+
+class Processor():
+    """ 
+        Processor for Skeleton-based Action Recgnition
+    """
+
+    def __init__(self, arg):
+        self.arg = arg
+        self.save_arg()
+        if arg.phase == 'train':
+            if not arg.train_feeder_args['debug']:
+                if os.path.isdir(arg.model_saved_name):
+                    print('log_dir: ', arg.model_saved_name, 'already exist')
+                    #answer = input('delete it? y/n:')
+                    answer = "n" # Just a hack to avoid the deadlock
+                    if answer == 'y':
+                        shutil.rmtree(arg.model_saved_name)
+                        print('Dir removed: ', arg.model_saved_name)
+                        # input('Refresh the website of tensorboard by pressing any keys')
+                    else:
+                        print('Dir not removed: ', arg.model_saved_name)
+                self.train_writer = SummaryWriter(os.path.join(arg.model_saved_name, 'train'), 'train')
+                self.val_writer = SummaryWriter(os.path.join(arg.model_saved_name, 'val'), 'val')
+        self.global_step = 0
+        self.load_model()
+        self.load_optimizer()
+        self.load_data()
+        self.lr = self.arg.base_lr
+        self.best_acc = 0
+        self.best_loss = 100
+
+    def load_data(self):
+        Feeder = import_class(self.arg.feeder)
+        self.data_loader = dict()
+        if self.arg.phase == 'train':
+            self.data_loader['train'] = torch.utils.data.DataLoader(
+                dataset=Feeder(**self.arg.train_feeder_args),
+                batch_size=self.arg.batch_size,
+                shuffle=True,
+                num_workers=self.arg.num_worker,
+                drop_last=True,
+                worker_init_fn=init_seed)
+        self.data_loader['test'] = torch.utils.data.DataLoader(
+            dataset=Feeder(**self.arg.test_feeder_args),
+            batch_size=self.arg.test_batch_size,
+            shuffle=False,
+            num_workers=self.arg.num_worker,
+            drop_last=False,
+            worker_init_fn=init_seed)
+
+    def load_model(self):
+        output_device = self.arg.device[0] if type(self.arg.device) is list else self.arg.device
+        self.output_device = output_device
+        Model = import_class(self.arg.model)
+        shutil.copy2(inspect.getfile(Model), self.arg.work_dir)
+        #print(Model)
+        self.model = Model(**self.arg.model_args).cuda(output_device)
+        self.model_hash = get_model_hash(self.model)
+        #print(self.model)
+        #num = sum(p.numel() for p in self.model.parameters())/1000/1000
+        #print(num)
+        wandb.watch(self.model)
+
+        self.loss = nn.CrossEntropyLoss().cuda(output_device)
+
+        if self.arg.weights:
+            self.global_step = int(arg.weights[:-3].split('-')[-1])
+            self.print_log('Load weights from {}.'.format(self.arg.weights))
+            if '.pkl' in self.arg.weights:
+                with open(self.arg.weights, 'r') as f:
+                    weights = pickle.load(f)
+            else:
+                weights = torch.load(self.arg.weights)
+
+            weights = OrderedDict(
+                [[k.split('module.')[-1],
+                  v.cuda(output_device)] for k, v in weights.items()])
+
+            for w in self.arg.ignore_weights:
+                if weights.pop(w, None) is not None:
+                    self.print_log('Sucessfully Remove Weights: {}.'.format(w))
+                else:
+                    self.print_log('Can Not Remove Weights: {}.'.format(w))
+
+            try:
+                self.model.load_state_dict(weights)
+            except:
+                state = self.model.state_dict()
+                diff = list(set(state.keys()).difference(set(weights.keys())))
+                print('Can not find these weights:')
+                for d in diff:
+                    print('  ' + d)
+                state.update(weights)
+                self.model.load_state_dict(state)
+        if type(self.arg.device) is list:
+            if len(self.arg.device) > 1:
+                self.model = nn.DataParallel(
+                    self.model,
+                    device_ids=self.arg.device,
+                    output_device=output_device)
+
+    def load_optimizer(self):
+        if self.arg.optimizer == 'SGD':
+            self.optimizer = optim.SGD(
+                self.model.parameters(),
+                lr=self.arg.base_lr,
+                momentum=0.9,
+                nesterov=self.arg.nesterov,
+                weight_decay=self.arg.weight_decay)
+        elif self.arg.optimizer == 'Adam':
+            self.optimizer = optim.Adam(
+                self.model.parameters(),
+                lr=self.arg.base_lr,
+                weight_decay=self.arg.weight_decay)
+        else:
+            raise ValueError()
+
+        self.lr_scheduler = ReduceLROnPlateau(self.optimizer, mode='min', factor=0.1,
+                                              patience=10, verbose=True,
+                                              threshold=1e-4, threshold_mode='rel',
+                                              cooldown=0)
+
+    def save_arg(self):
+        # save arg
+        arg_dict = vars(self.arg)
+        if not os.path.exists(self.arg.work_dir):
+            os.makedirs(self.arg.work_dir)
+        with open('{}/config.yaml'.format(self.arg.work_dir), 'w') as f:
+            yaml.dump(arg_dict, f)
+
+    def adjust_learning_rate(self, epoch):
+        if self.arg.optimizer == 'SGD' or self.arg.optimizer == 'Adam':
+            if epoch < self.arg.warm_up_epoch:
+                lr = self.arg.base_lr * (epoch + 1) / self.arg.warm_up_epoch
+            else:
+                lr = self.arg.base_lr * (
+                        0.1 ** np.sum(epoch >= np.array(self.arg.step)))
+            for param_group in self.optimizer.param_groups:
+                param_group['lr'] = lr
+            return lr
+        else:
+            raise ValueError()
+
+    def print_time(self):
+        localtime = time.asctime(time.localtime(time.time()))
+        self.print_log("Local current time :  " + localtime)
+
+    def print_log(self, str, print_time=True):
+        if print_time:
+            localtime = time.asctime(time.localtime(time.time()))
+            str = "[ " + localtime + ' ] ' + str
+        print(str)
+        if self.arg.print_log:
+            with open('{}/log.txt'.format(self.arg.work_dir), 'a') as f:
+                print(str, file=f)
+
+    def record_time(self):
+        self.cur_time = time.time()
+        return self.cur_time
+
+    def split_time(self):
+        split_time = time.time() - self.cur_time
+        self.record_time()
+        return split_time
+ 
+    def start(self):
+        self.print_log('Parameters:\n{}\n'.format(str(vars(self.arg))))
+        scores = calculate_zc_proxy_scores(self.model, self.data_loader['train'], self.output_device, self.loss, self.model_hash)
+        print(scores)
+            
+            # with open('architectures/generated_architectures.json', 'r') as f:
+            #     architectures = json.load(f)
+            #     architectures[self.model_hash]["val_acc"] = self.best_acc
+            #     architectures[self.model_hash]["time"] = end_time - start_time 
+            
+            # with open('architectures/generated_architectures.json', 'w') as f:
+            #     json.dump(architectures, f)
+            
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def import_class(name):
+    components = name.split('.')
+    mod = __import__(components[0])  # import return model
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    # load arg form config file
+    p = parser.parse_args()
+    if p.config is not None:
+        with open(p.config, 'r') as f:
+            #default_arg = yaml.load(f)
+            default_arg = yaml.load(f, Loader=yaml.FullLoader)
+        key = vars(p).keys()
+        for k in default_arg.keys():
+            if k not in key:
+                print('WRONG ARG: {}'.format(k))
+                assert (k in key)
+        parser.set_defaults(**default_arg)
+
+    arg = parser.parse_args()
+    init_seed(0)
+    processor = Processor(arg)
+    processor.start()
