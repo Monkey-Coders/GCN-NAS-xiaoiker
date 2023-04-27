a3f7697d9536feb372a2ed1bd11888024d7e2151
Zaim Imran <zaim.imran@gmail.com>
2023-04-27 02:23:39 +0200

    a

---
+++ utils/retrain_diff.py
@@ -0,0 +1,18 @@
+import json
+
+with open("experiment/generated_architectures.json", "r") as f:
+    architectures = json.load(f)
+
+with open("experiment/generated_architectures_old.json", "r") as f:
+    architectures_old = json.load(f)
+    
+count = 0
+for model_hash, model in architectures.items():
+    if architectures_old[model_hash]["val_acc"] != model["val_acc"]:
+        print(f"# 'layers' : {len(model['weights'])}")
+        print(f"\033[92m+ 'val_acc': {model['val_acc']}\033[0m")
+        print(f"\033[91m- 'val_acc': {architectures_old[model_hash]['val_acc']}\033[0m")
+        count += 1
+print()
+print(f"Total: {count}")
+
